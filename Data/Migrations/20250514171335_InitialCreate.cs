dd0481405c997845da54cd3faca64b1a53fadff1
JuliaAndreyeva <blackonblackua@gmail.com>
2025-05-14 20:31:01 +0300

    fix: Added new migrations

---
+++ Data/Migrations/20250514171335_InitialCreate.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    DateOfDeath = table.Column<DateOnly>(type: "date", nullable: true),
+                    Biography = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LiteratureDirections",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartCentury = table.Column<long>(type: "bigint", nullable: false),
+                    EndCentury = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiteratureDirections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Occupations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Occupations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publications",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicationYear = table.Column<DateOnly>(type: "date", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorLiteratureDirection",
+                columns: table => new
+                {
+                    AuthorsId = table.Column<long>(type: "bigint", nullable: false),
+                    LiteratureDirectionId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorLiteratureDirection", x => new { x.AuthorsId, x.LiteratureDirectionId });
+                    table.ForeignKey(
+                        name: "FK_AuthorLiteratureDirection_Authors_AuthorsId",
+                        column: x => x.AuthorsId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorLiteratureDirection_LiteratureDirections_LiteratureDirectionId",
+                        column: x => x.LiteratureDirectionId,
+                        principalTable: "LiteratureDirections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorOccupation",
+                columns: table => new
+                {
+                    AuthorsId = table.Column<long>(type: "bigint", nullable: false),
+                    OccupationsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorOccupation", x => new { x.AuthorsId, x.OccupationsId });
+                    table.ForeignKey(
+                        name: "FK_AuthorOccupation_Authors_AuthorsId",
+                        column: x => x.AuthorsId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorOccupation_Occupations_OccupationsId",
+                        column: x => x.OccupationsId,
+                        principalTable: "Occupations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorOrganization",
+                columns: table => new
+                {
+                    AuthorsId = table.Column<long>(type: "bigint", nullable: false),
+                    OrganizationsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorOrganization", x => new { x.AuthorsId, x.OrganizationsId });
+                    table.ForeignKey(
+                        name: "FK_AuthorOrganization_Authors_AuthorsId",
+                        column: x => x.AuthorsId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorOrganization_Organizations_OrganizationsId",
+                        column: x => x.OrganizationsId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorPublication",
+                columns: table => new
+                {
+                    AuthorsId = table.Column<long>(type: "bigint", nullable: false),
+                    PublicationsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorPublication", x => new { x.AuthorsId, x.PublicationsId });
+                    table.ForeignKey(
+                        name: "FK_AuthorPublication_Authors_AuthorsId",
+                        column: x => x.AuthorsId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorPublication_Publications_PublicationsId",
+                        column: x => x.PublicationsId,
+                        principalTable: "Publications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LiteratureDirectionPublication",
+                columns: table => new
+                {
+                    LiteratureDirectionId = table.Column<long>(type: "bigint", nullable: false),
+                    PublicationsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiteratureDirectionPublication", x => new { x.LiteratureDirectionId, x.PublicationsId });
+                    table.ForeignKey(
+                        name: "FK_LiteratureDirectionPublication_LiteratureDirections_LiteratureDirectionId",
+                        column: x => x.LiteratureDirectionId,
+                        principalTable: "LiteratureDirections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LiteratureDirectionPublication_Publications_PublicationsId",
+                        column: x => x.PublicationsId,
+                        principalTable: "Publications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    PhotoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(21)", maxLength: 21, nullable: false),
+                    AuthorId = table.Column<long>(type: "bigint", nullable: true),
+                    Quote = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PublicationId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Photos_Publications_PublicationId",
+                        column: x => x.PublicationId,
+                        principalTable: "Publications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PublicationTag",
+                columns: table => new
+                {
+                    PublicationsId = table.Column<long>(type: "bigint", nullable: false),
+                    TagsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicationTag", x => new { x.PublicationsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_PublicationTag_Publications_PublicationsId",
+                        column: x => x.PublicationsId,
+                        principalTable: "Publications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PublicationTag_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorLiteratureDirection_LiteratureDirectionId",
+                table: "AuthorLiteratureDirection",
+                column: "LiteratureDirectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorOccupation_OccupationsId",
+                table: "AuthorOccupation",
+                column: "OccupationsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorOrganization_OrganizationsId",
+                table: "AuthorOrganization",
+                column: "OrganizationsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorPublication_PublicationsId",
+                table: "AuthorPublication",
+                column: "PublicationsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LiteratureDirectionPublication_PublicationsId",
+                table: "LiteratureDirectionPublication",
+                column: "PublicationsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AuthorId",
+                table: "Photos",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_PublicationId",
+                table: "Photos",
+                column: "PublicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicationTag_TagsId",
+                table: "PublicationTag",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorLiteratureDirection");
+
+            migrationBuilder.DropTable(
+                name: "AuthorOccupation");
+
+            migrationBuilder.DropTable(
+                name: "AuthorOrganization");
+
+            migrationBuilder.DropTable(
+                name: "AuthorPublication");
+
+            migrationBuilder.DropTable(
+                name: "LiteratureDirectionPublication");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "PublicationTag");
+
+            migrationBuilder.DropTable(
+                name: "Occupations");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+
+            migrationBuilder.DropTable(
+                name: "LiteratureDirections");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Publications");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
